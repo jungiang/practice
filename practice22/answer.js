d4888086ac61ab135649a8bc8df29bab673a697f
Jun Giang <jungiang@yahoo.com>
2019-09-19 11:15:24 -0700

    finished practice problems level 2

--- practice22/answer.js
+++ practice22/answer.js
@@ -1,1 +1,15 @@
-answer.js
+function sort_object_list_by_field(input_array, sort_field){
+    var swaps = 0;
+    for(var i = 0; i < input_array.length - 1; i++){
+        if(input_array[i][sort_field] > input_array[i+1][sort_field]){
+            var variable_placeholder = input_array[i];
+            input_array[i] = input_array[i+1];
+            input_array[i+1] = variable_placeholder;
+            swaps++;
+        }
+    }
+    if(swaps > 0){
+        sort_object_list_by_field(input_array, sort_field);
+    }
+    return input_array;
+}
--- practice26/answer.js
+++ practice26/answer.js
@@ -1,1 +1,12 @@
-answer.js
+function search_array(haystack_array, needle_array){
+    var output_array = [];
+    for(var i = 0; i < needle_array.length; i++){
+        for(var j = 0; j < haystack_array.length; j++){
+            if(haystack_array[j] === needle_array[i]){
+                output_array.push(needle_array[i]);
+                break;
+            }
+        }
+    }
+    return output_array;
+} 
